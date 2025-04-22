0e58943115b2172b64215f0d73fbda3091196ff8
DayDayYES <2412492703@qq.com>
2025-04-22 12:59:52 +0800

    Task

---
+++ Task.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 24124 on 2025-04-22.
+//
+
+#include "Task.h"
---
+++ Task.h
@@ -0,0 +1,16 @@
+//
+// Created by 24124 on 2025-04-22.
+//
+
+#ifndef TASK_H
+#define TASK_H
+
+
+
+class Task {
+
+};
+
+
+
+#endif //TASK_H
