c04d6198f92e4322432e1798cef30efc3802162c
DayDayYES <2412492703@qq.com>
2025-04-22 20:03:05 +0800

    finish

---
+++ Command.h
@@ -0,0 +1,16 @@
+//
+// Created by 24124 on 2025-04-22.
+//
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+
+
+class Command {
+
+};
+
+
+
+#endif //COMMAND_H
---
+++ TaskManger.h
@@ -0,0 +1,16 @@
+//
+// Created by 24124 on 2025-04-22.
+//
+
+#ifndef TASKMANGER_H
+#define TASKMANGER_H
+
+
+
+class TaskManger {
+
+};
+
+
+
+#endif //TASKMANGER_H
